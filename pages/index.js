5a4b6d85317a13329676b4716895d6fbfab7cbd0
Magrgo <102856537+Magrgo@users.noreply.github.com>
2022-04-02 12:13:07 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Köszöntelek az oldalamon!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Magyar Gergő -- 2022</code>
         </p>
       </main>
 
